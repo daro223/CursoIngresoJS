20fdedff8952671b5c2d3669f00dd8ba41ad846a
daro223 <daro2234@gmail.com>
2017-07-14 17:40:12 -0300

    TP RisingBTL en marcha

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,63 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+   var edad;
+   var sexo;
+   var estado;
+   var sueldo;
+   var legajo;
+   var nacionalidad;
+
+	//limita la edad entre 18 y 90, pero acepta negativos
+	edad=prompt("Ingrese una edad entre 18 y 90 años.");
+	while((edad>90||edad<18)==(/^([0-9])*$/.test(edad)))
+	{
+		alert("El valor '" + edad + "' no es valido.");
+		edad=prompt("Ingrese una edad entre 18 y 90 años.");
+	}
+	validacion=document.getElementById('Edad').value=edad;		
+	
+	sexo=prompt("Ingrese 'f' para Femenino ó 'm' para masculino.")
+	while(sexo!="f"&&sexo!="m")
+	{
+		alert("Usted no es hombre ni mujer.");
+		sexo=prompt("Ingrese 'f' ó 'm'.");
+	}	
+	if(sexo=="f")
+	{
+		document.getElementById('Sexo').value="Femenino.";		
+	}
+	else
+	{
+		document.getElementById('Sexo').value="Masculino.";			
+	}
+
+	estado=prompt("Ingrese \n1-para soltero/a,\n2-para casado/a, \n3-para divorciado/a, \n4-para viudo/a.");
+		/*Estado civil, 
+		1-para soltero,
+		2-para casados, 
+		3-para divorciados y 
+		4-para viudos */
+	/*while((estado<1||estado>4)==(/^([0-9])*$/.test(estado)))
+	{
+		alert("Número incorrecto");
+		estado=prompt("Ingrese \n1-para soltero/a,\n2-para casado/a, \n3-para divorciado/a, \n4-para viudo/a.");
+	}*/
+	switch(estado)
+	{
+		case 1:
+		estado="Soltero/a";
+		break;
+		case 2:
+		estado="casado/a";
+		break;
+		case 3:
+		estado="divorciado/a";
+		break;
+		case 4:
+		estado="viudo/a";
+		break;
+	}
+	document.getElementById('EstadoCivil').value=estado;
+	alert(estado);
 }
