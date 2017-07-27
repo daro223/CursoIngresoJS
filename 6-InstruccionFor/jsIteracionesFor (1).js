da6a5ddc5fc265ed067b89caba660a58057631f8
daro223 <daro2234@gmail.com>
2017-07-26 22:05:27 -0300

    primer commit

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
+	contador=1
+	respuesta="SI"
+	for( ;respuesta=="SI" ; )
+	{
+		console.log("Hola " + contador);
+		if(contador==10)
+		{
+			break;
+		}
+		contador++;
+		respuesta=prompt();
+
+
+	}
+
+
+
 
 }
